857d6c134015783e312c9d24eeb2d37074740fec
David Garcia Latorre <dgarcia@seocom.agency>
2021-01-17 14:27:45 +0100

    Initial setup

---
+++ src/main/kotlin/com/sultanicq/firstwebsite/FirstwebsiteApplication.kt
@@ -0,0 +1,11 @@
+package com.sultanicq.firstwebsite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstwebsiteApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstwebsiteApplication>(*args)
+}
---
+++ src/test/kotlin/com/sultanicq/firstwebsite/FirstwebsiteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sultanicq.firstwebsite
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FirstwebsiteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
